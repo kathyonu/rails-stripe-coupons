a42e9b2a5346ff90820b9b6c11880d1bb51a3c8b
Will Schive <lexigram@goodworksonearth.org>
2015-11-10 07:37:34 -0800

    rubcop houseclean

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
   factory :user do
-    email "test@example.com"
-    password "please123"
-    role "admin"
-#    trait :admin do
- #     role 'admin'
-  #  end
+    email 'test@example.com'
+    password 'please123'
+    role 'admin'
 
+    trait :admin do
+      role 'admin'
+    end
   end
 end
