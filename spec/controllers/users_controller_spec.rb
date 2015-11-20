e19c7bb968ec386fefc7c70359ca7fff09d270d5
Will Schive <lexigram@goodworksonearth.org>
2015-11-19 18:22:06 -0800

    users controller test pass

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,33 +1,31 @@
 include Warden::Test::Helpers
 Warden.test_mode!
- 
-describe UsersController do
 
+describe UsersController do
   after(:each) do
     Warden.test_reset!
   end
 
-  describe "GET #index" do
-
-    it "renders the :index view for Admin" do
+  describe 'GET #index' do
+    it 'renders the :index view for Admin' do
       admin = FactoryGirl.create(:user, role: 'admin')
       sign_in(admin)
       get :index
       expect(response).to render_template :index
     end
 
-    it "renders the :index view for User" do
+    it 'renders the :index view for User' do
       user = FactoryGirl.create(:user, email: 'newuser@example.com')
       sign_in(user)
       get :index
       expect(response).to render_template :index
     end
 
-    it "populates an array of users" do 
+    it 'populates an array of users' do
       admin = FactoryGirl.create(:user, role: 'admin')
       sign_in(admin)
       get :index
-      expect(assigns(:users)).to eq([admin]) 
+      expect(assigns(:users)).to eq([admin])
     end
   end
 end
